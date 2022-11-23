fc43c5eac12261cfdd6ac04b3cdf4657ee03342a
Denis <grandeni152>
2022-11-23 13:53:56 +0300

    feat: wrap app to class

--- src/index.ts
+++ src/index.ts
@@ -6,91 +6,116 @@ import express, {
   Response,
   urlencoded,
 } from "express";
-import { WebSocket } from "ws";
+import { Server, WebSocket } from "ws";
 import http from "http";
+import https from "https";
 import helmet from "helmet";
 import cors from "cors";
 import requestIp from "request-ip";
 import createError, { HttpError } from "http-errors";
 import bodyParser from "body-parser";
 
-import { logMiddleware } from "./logger/log.middleware";
 import * as locals from "./locals";
+import { logMiddleware } from "./logger/log.middleware";
 import wsRouter from "./apis/wss.route";
 import { disconnectHelper, sendToDb } from "./helpers";
 
-const app = express();
-const pinoLogger = logMiddleware("wss");
-const server = http.createServer(app);
+export class Service {
+  declare readonly port: number;
+  declare readonly key: string;
+  declare readonly cert: string;
 
-app.use(helmet());
-app.use(json());
-app.use(cors());
-app.use(bodyParser.json());
-app.use(urlencoded({ extended: false }));
-app.use(requestIp.mw({ attributeName: "ip" }));
-app.use(pinoLogger);
+  declare app: any;
+  declare server: https.Server | http.Server;
+  declare wss: Server<WebSocket>;
+  declare clients: Record<any, any>;
 
-app.use("/", wsRouter);
-
-const ws = new WebSocket.Server({ server });
-app.set("socket", ws);
-
-const clients = {};
-
-const bootstrap = async () => {
-  try {
-    ws.on("connection", (ws: WebSocket) => {
-      const id = randomUUID();
-      clients[id] = ws;
-
-      ws.on("message", (msg: string) => {
-        console.log("receive:", msg.toString());
-        ws.send(`${msg} was sent`);
-        sendToDb(id, msg.toString());
-      });
-
-      ws.on("close", () => {
-        delete clients[id];
-        disconnectHelper(id);
-        console.log(`${id} left`);
-      });
-
-      ws.send("websocket server connected");
-    });
-
-    ws.on("message", (msg) => console.log(msg));
-
-    server.listen(locals.PORT, () =>
-      console.log(`server started on ${locals.PORT}`)
-    );
-
-    process.on("SIGINT", () => {
-      ws.close();
-      process.exit();
-    });
-  } catch (e) {
-    console.log(e);
+  constructor(port: number, key?: string, cert?: string) {
+    this.port = port;
+    this.key = key;
+    this.cert = cert;
+    this.app = express();
+    this.server =
+      this.key && this.cert
+        ? https.createServer({ key: this.key, cert: this.cert }, this.app)
+        : http.createServer(this.app);
+    this.wss = new WebSocket.Server({ server: this.server });
   }
-};
 
-bootstrap();
+  public async run() {
+    const app = this.app;
+    const pinoLogger = logMiddleware("wss");
+    const server = this.server;
 
-app.use(function (req, res, next) {
-  next(createError(404));
-});
+    app.use(helmet());
+    app.use(json());
+    app.use(cors());
+    app.use(bodyParser.json());
+    app.use(urlencoded({ extended: false }));
+    app.use(requestIp.mw({ attributeName: "ip" }));
+    app.use(pinoLogger);
 
-app.use(function (
-  err: HttpError,
-  req: Request,
-  res: Response,
-  next: NextFunction
-) {
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+    app.use("/", wsRouter);
 
-  res.status(err.status || 500);
-  res.json({
-    error: `${err.name}: ${err.message}`,
-  });
-});
+    const wss = this.wss;
+
+    this.clients = {};
+
+    try {
+      wss.on("connection", (ws: WebSocket) => {
+        const id = randomUUID();
+        this.clients[id] = ws;
+
+        ws.on("message", (msg: string) => {
+          console.log("receive:", msg.toString());
+          ws.send(`${msg} was sent`);
+          sendToDb(id, msg.toString());
+        });
+
+        ws.on("close", () => {
+          delete this.clients[id];
+          disconnectHelper(id);
+          console.log(`${id} left`);
+        });
+
+        ws.send("websocket server connected");
+      });
+
+      wss.on("message", (msg) => console.log(msg));
+
+      server.listen(this.port, () =>
+        console.log(`server started on ${this.port}`)
+      );
+
+      app.use(function (req, res, next) {
+        next(createError(404));
+      });
+
+      app.use(function (
+        err: HttpError,
+        req: Request,
+        res: Response,
+        next: NextFunction
+      ) {
+        res.locals.message = err.message;
+        res.locals.error = req.app.get("env") === "development" ? err : {};
+
+        res.status(err.status || 500);
+        res.json({
+          error: `${err.name}: ${err.message}`,
+        });
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  public destroy() {
+    this.wss.close();
+    this.server.close(() => process.exit());
+    process.exit();
+  }
+}
+
+const service = new Service(locals.PORT);
+service.run();
