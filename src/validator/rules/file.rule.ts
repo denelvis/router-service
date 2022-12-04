3c67e2391d3793523a8c5bd4317e29f4e9bab3eb
Denis <denelvis@yandex.ru>
2022-12-04 21:25:47 +0300

    refactor: update body validator for video

--- src/validator/rules/file.rule.ts
+++ src/validator/rules/file.rule.ts
@@ -1,5 +1,13 @@
 export const fileRules = {
-  name: ["required", "string"],
-  extName: ["required", "string"],
+  id: ["required", "string"],
+  infos: ["required", "object"],
+  'infos.dir': ["required", "string"],
+  'infos.masterSwarmId': ["required", "string"],
+  'infos.segments': ["required", "object"],
+  'infos.thumbnail': ["required", "string"],
+  'infos.trackerUrls': ["required", "array"],
+  'infos.videoDetails': ["required", "object"],
   size: ["required", "integer"],
+  video: ["required", "object"],
+  'video.internalURL': ["required", "string"],
 };
